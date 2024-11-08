5a7a2d385821799cfcad1941b463227e778e521d
Milan Soucek <milan.soucek.2004@gmail.com>
2024-11-09 00:54:43 +0100

    propojení s MongoDB

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,32 @@
-import app from './app.js';
-const PORT = process.env.PORT || 3000;
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+const app = express();
+mongoose.set('strictQuery', false);
 
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+if (process.env.NODE_ENV !== 'development') {
+    dotenv.config({path:'config/.env'});
+}
+
+const PORT = process.env.PORT || 3000;
+const CONNECTION = process.env.CONNECTION;
+
+
+const start = async() => {
+    try {
+        await mongoose.connect(CONNECTION);
+
+        app.listen(PORT, () => {
+            console.log(`Server started on port ${PORT}`);
+        });
+    } catch (e) {
+        console.log(e.message);
+    }
+};
+
+start();
 
