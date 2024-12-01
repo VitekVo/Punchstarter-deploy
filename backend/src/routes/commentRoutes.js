8d71d62dfbbf9f52dcca3095ddf87ae69a4da0d4
infamous-coldcoconut <michalpham2004@gmail.com>
2024-12-01 17:49:12 +0100

    update in routes

--- backend/src/routes/commentRoutes.js
+++ backend/src/routes/commentRoutes.js
@@ -1,14 +1,16 @@
-import express from  'express';
+import express from "express";
 
-import { createComment, getCommentsByProjectId, updateComment, deleteComment } from '../controllers/commentController.js';
+import createComment from "../controllers/commentController/createComment.js";
+import getCommentsByProjectId from "../controllers/commentController/getCommentsByProjectId.js";
+import updateComment from "../controllers/commentController/updateComment.js";
+import deleteComment from "../controllers/commentController/deleteComment.js";
 
+const router = express.Router();
 
- const router = express.Router();
-
- router.post('/', createComment);
- router.get('/:projectId', getCommentsByProjectId);
- router.put('/:commentId', updateComment);
- router.delete('/:commentId', deleteComment);
+router.post("/", createComment);
+router.get("/:projectId", getCommentsByProjectId);
+router.put("/:commentId", updateComment);
+router.delete("/:commentId", deleteComment);
 //router.post('/:projectId/comments/:commentId', addCommentToProject);
 
 export default router;
