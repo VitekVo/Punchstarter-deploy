bff8d01865f8eb30d5c1d6f6c6550a7ce0788314
Makovicee <martinex.husnik@seznam.cz>
2024-12-04 22:36:45 +0100

    feat(projectDetail):adding comments to BE now possible

--- ps-app/src/app/(main)/detail/[projectID]/projectContent/scrollBoxContent/projectComments.tsx
+++ ps-app/src/app/(main)/detail/[projectID]/projectContent/scrollBoxContent/projectComments.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { use } from "react";
 import { IProject } from "@/utils/types/types";
 import Button from "@/components/button/Button";
 import { Comment } from "@/components/comment/Comment";
@@ -8,15 +8,40 @@ import { comment } from "postcss";
 import { RiH1 } from "react-icons/ri";
 const ProjectComments = ({ project }: { project: IProject }) => {
   const [showAddComment, setShowAddComment] = useState(false);
-  const [comments, setComments] = useState(project.comments); // TODO: je to příšerný řešení, dělal jsem to rychle na hodinu PM :)
-  function handleAddComment(newComment: string) {
-    setComments([...comments, { comment: newComment }]);
+  const [comments, setComments] = useState(project.comments);
+
+  async function handleAddComment(newComment: string) {
+    const commentData = {
+      project_id: project._id,
+      comment: newComment,
+      user_id: "6750a2d0999e49b6f5713d48", //TODO: add current logged in user when userProvider is ready
+    };
+
+    console.log(commentData);
+
+    const response = await fetch("http://localhost:2580/projects/addComment", {
+      method: "POST",
+      body: JSON.stringify(commentData),
+      headers: { "Content-Type": "application/json" },
+    });
+
+    const json = await response.json();
+    if (!response.ok) {
+      console.log(json.error);
+    }
+    if (response.ok) {
+      console.log("new list created", json);
+    }
+    console.log(comments);
+    console.log(json.comment);
+    setComments([...comments, json.comment]);
+    setShowAddComment(false);
   }
   return (
     <div id={"comments"} className={"carousel-item w-full"}>
       <div
         className={
-          "w-full h-full border rounded-lg flex flex-col p-8 px-12 text-gray-700"
+          "w-full h-fit border rounded-lg flex flex-col p-8 px-12 text-gray-700"
         }
       >
         <div className="grid grid-cols-7">
@@ -32,8 +57,8 @@ const ProjectComments = ({ project }: { project: IProject }) => {
           {showAddComment && (
             <AddComment handleAddComment={handleAddComment}></AddComment>
           )}
-          {project.comments.length > 0 ? (
-            project.comments.map((comment) => (
+          {comments.length > 0 ? (
+            comments.map((comment) => (
               <Comment
                 key={comment._id}
                 _id={comment._id}
