176ea4ca7bdeff15ce0bc9506ce2c3978184c530
Vojta Vitek <vojta0211@gmail.com>
2024-12-11 23:39:47 +0100

    trying to fix port

--- backend/src/server.js
+++ backend/src/server.js
@@ -21,7 +21,7 @@ app.use(
   cors({
     origin: "http://localhost:3000",
     credentials: true,
-  }),
+  })
 );
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -31,7 +31,7 @@ if (process.env.NODE_ENV !== "development") {
   dotenv.config({ path: "./src/config/.env" });
 }
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 const CONNECTION = process.env.CONNECTION;
 
 app.use("*", checkUser);
