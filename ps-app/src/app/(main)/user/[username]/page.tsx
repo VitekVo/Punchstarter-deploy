18a58797b00095f687131fc75fdaf351ddd0a115
Jan Zdenek <ydenekjan@gmail.com>
2025-01-07 14:28:53 +0100

    feat(projects): deleting projects

--- ps-app/src/app/(main)/user/[username]/page.tsx
+++ ps-app/src/app/(main)/user/[username]/page.tsx
@@ -22,8 +22,6 @@ const Page = () => {
 function ListConsumerComponent() {
   const { listsData } = useListContext();
   const { user } = useUserContext();
-  const params = useParams();
-  const { username } = params;
 
   const filteredLists = listsData?.projects.filter(
     (list) => String(list.creatorId._id) === String(user?.id),
@@ -35,7 +33,7 @@ function ListConsumerComponent() {
 
   return (
     <>
-      {user?.username === username && (
+      {user?.username && (
         <>
           <ProjectCarousel
             title={`My Projects (${filteredLists?.length || 0})`}
