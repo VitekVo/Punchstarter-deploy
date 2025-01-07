d6f0094fdcb98b034f78102b04f9f5106babe9e2
Makovicee <martinex.husnik@seznam.cz>
2025-01-07 10:29:25 +0100

    responsive + avatar

--- ps-app/src/app/(main)/detail/[projectID]/projectContent/scrollBoxContent/projectContent.tsx
+++ ps-app/src/app/(main)/detail/[projectID]/projectContent/scrollBoxContent/projectContent.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { IProject } from "@/utils/types/types";
 import Image from "next/image";
+import Avatar from "@/components/navbar/navElements/avatar";
 const ProjectContent = ({ project }: { project: IProject }) => {
   return (
     <div className="grid lg:grid-cols-3 grid-cols-1 grid-rows-2 lg:grid-rows-1 gap-8 h-full carousel-item w-full max-w-">
@@ -8,19 +9,7 @@ const ProjectContent = ({ project }: { project: IProject }) => {
         <h1 className="text-2xl uppercase font-bold pt-6 px-8">Story</h1>
         <div
           className={"relative w-full overflow-hidden flex items-center h-52"}
-        >
-          <Image
-            className={"object-cover w-full h-auto"}
-            alt={"Shoes"}
-            src={
-              "https://img.daisyui.com/images/stock/photo-1625726411847-8cbb60cc71e6.webp"
-            }
-            layout="responsive"
-            width={100}
-            height={100}
-            objectFit="cover"
-          />
-        </div>
+        ></div>
 
         <div className="flex items-stretch">
           <p className="px-8 pb-12">{project.description}</p>
@@ -28,10 +17,10 @@ const ProjectContent = ({ project }: { project: IProject }) => {
       </div>
       <div className="flex flex-col h-fit  border rounded-lg gap-4 pt-16 p-8">
         <header className="border rounded-md flex flex-col items-center py-4 relative pt-12 gap-1">
-          <div className="rounded-full aspect-square w-20 bg-neutral-300 border-4 absolute top-0 -translate-y-1/2 flex items-center justify-center overflow-hidden">
+          <div className="rounded-full aspect-square w-20  absolute top-0 -translate-y-1/2 flex items-center justify-center overflow-hidden">
             <div className="avatar">
               <div className="w-30 rounded-full">
-                <img src="/CandiceLigma.jpg" alt="Avatar" />
+                <Avatar username={project.creatorId.username} />
               </div>
             </div>
           </div>
